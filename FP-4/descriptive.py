60737bbade8531a96e3bb8690ffc3da08db3a5f5
anpanx10 <moriu087@gmail.com>
2025-11-27 21:55:47 +0900

    add plots function

--- FP-4/descriptive.py
+++ FP-4/descriptive.py
@@ -63,3 +63,53 @@ def display_dispersion_table(num=1):
     row_labels_dispersion = 'st.dev.', 'min', 'max', 'range', '25th', '75th', 'IQR'
     df_dispersion.index   = row_labels_dispersion
     display( df_dispersion )
+
+def plots(x):
+
+    import parse_data as parse
+    df = parse.parse_file('healthassessment.csv')
+    df = df.dropna(subset = ['denominator', 'number_with_outcome'])
+    
+    df_access = df[['number_with_outcome']].where( df.topic == 'ACCESS TO CARE' )
+    df_death = df[['number_with_outcome']].where( df.topic == 'DEATH' )
+    df_dental = df[['number_with_outcome']].where( df.topic == 'DENTAL' )
+    df_healthcond = df[['number_with_outcome']].where( df.topic == 'HEALTH CONDITION' )
+    df_healthbehav = df[['number_with_outcome']].where( df.topic == 'HEALTH BEHAVIOR' )
+    df_nutrition = df[['number_with_outcome']].where( df.topic == 'NUTRITION' )
+    df_safety = df[['number_with_outcome']].where( df.topic == 'SAFETY' )
+    df_socialdeterm = df[['number_with_outcome']].where( df.topic == 'SOCIAL DETERMINANT' )
+    df_substance = df[['number_with_outcome']].where( df.topic == 'SUBSTANCE USE' )
+    
+    fig,axs = plt.subplots( 3, 3, figsize=(10,8), tight_layout=False )
+    
+    axs[0,0].scatter( df[['year']], df_access, alpha=0.5, color='b' )
+    axs[0,0].set_xlabel('Access to care')
+    
+    axs[0,1].scatter( df[['year']], df_death, alpha=0.5, color='r' )
+    axs[0,1].set_xlabel('Death')
+    
+    axs[0,2].scatter( df[['year']], df_dental, alpha=0.5, color='g' )
+    axs[0,2].set_xlabel('Dental')
+    
+    axs[1,0].scatter( df[['year']], df_healthcond, alpha=0.5, color='r' )
+    axs[1,0].set_xlabel('Health condition')
+    
+    axs[1,1].scatter( df[['year']], df_healthbehav, alpha=0.5, color='g' )
+    axs[1,1].set_xlabel('Health behavior')
+    
+    axs[1,2].scatter( df[['year']], df_nutrition, alpha=0.5, color='b' )
+    axs[1,2].set_xlabel('Nutrition')
+    
+    axs[2,0].scatter( df[['year']], df_safety, alpha=0.5, color='r' )
+    axs[2,0].set_xlabel('Safety')
+    
+    axs[2,1].scatter( df[['year']], df_socialdeterm, alpha=0.5, color='g' )
+    axs[2,1].set_xlabel('Social determinants')
+    
+    axs[2,2].scatter( df[['year']], df_substance, alpha=0.5, color='g' )
+    axs[2,2].set_xlabel('Substance use')
+    
+    fig.supxlabel('Year')
+    fig.supylabel('Number of cases with outcome')
+    
+    plt.show()
